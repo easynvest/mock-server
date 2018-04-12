0d966db15d2ea50f5d4a6f081a62c17858e0de81
Wellyngton Amaral <wellington.amaral.ufc@gmail.com>
2018-04-12 15:20:51 -0300

    Create delay option for request

--- middlewares/mock.js
+++ middlewares/mock.js
@@ -31,72 +31,79 @@ module.exports = ({ server, dbService }) => async (req, res, next) => {
   const method = req.method;
   const uri = `http://${URI_API}${parsedUrl.path}`;
   let mockRequest = dbService.onRequests.getTo({ method, url: uri });
+  let delay = 0;
 
-  if (!server.locals.requestApi) {
-    res.append("x-request-mock", "true");
+	if(mockRequest) {
+		delay = mockRequest.delay || 0;
+	}
+	
+	setTimeout(async ()=>{
+		if (!server.locals.requestApi) {
+			res.append("x-request-mock", "true");
 
-    res.status(mockRequest.status);
-    res.send(mockRequest.response);
-    return;
-  }
+			res.status(mockRequest.status);
+			res.send(mockRequest.response);
+			return;
+		}
 
-  const { authorization, "content-type": contentType = "" } = req.headers;
+		const { authorization, "content-type": contentType = "" } = req.headers;
 
-  try {
-    let reqBody = req.body;
-    if (contentType.includes("json")) {
-      reqBody = JSON.stringify(reqBody);
-    }
+		try {
+			let reqBody = req.body;
+			if (contentType.includes("json")) {
+				reqBody = JSON.stringify(reqBody);
+			}
 
-    if (contentType.includes("x-www-form-urlencoded")) {
-      const form = new url.URLSearchParams();
-      Object.keys(req.body).forEach(key => {
-        form.append(key, req.body[key]);
-      });
+			if (contentType.includes("x-www-form-urlencoded")) {
+				const form = new url.URLSearchParams();
+				Object.keys(req.body).forEach(key => {
+					form.append(key, req.body[key]);
+				});
 
-      reqBody = form;
-    }
+				reqBody = form;
+			}
 
-    const config = {
-      method,
-      body: reqBody,
-      headers: {
-        authorization,
-        "content-type": contentType
-      }
-    };
+			const config = {
+				method,
+				body: reqBody,
+				headers: {
+					authorization,
+					"content-type": contentType
+				}
+			};
 
-    const request = await fetch(uri, config);
+			const request = await fetch(uri, config);
 
-    if ([200, 400, 204].indexOf(request.status) === -1) {
-      res.append("x-request-mock", "true");
+			if ([200, 400, 204].indexOf(request.status) === -1) {
+				res.append("x-request-mock", "true");
 
-      res.status(mockRequest.status);
-      res.send(mockRequest.response);
-      return;
-    }
+				res.status(mockRequest.status);
+				res.send(mockRequest.response);
+				return;
+			}
 
-    res.set("Content-Type", "application/json");
+			res.set("Content-Type", "application/json");
 
-    let json
-    try{
-      json = await request.json();
-    }catch(e){
-      console.log(e)
-    }
+			let json
+			try{
+				json = await request.json();
+			}catch(e){
+				console.log(e)
+			}
 
-    saveRequestDb({mockRequest}, {
-      type: "default",
-      method,
-      url: uri,
-      status: request.status,
-      response: json
-    });
+			saveRequestDb({mockRequest}, {
+				type: "default",
+				method,
+				url: uri,
+				status: request.status,
+				response: json
+			});
 
-    res.status(request.status);
-    res.send(json);
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ err: 1 });
-  }
+			res.status(request.status);
+			res.send(json);
+		} catch (e) {
+			console.log(e);
+			res.status(500).json({ err: 1 });
+		}
+	},delay)
 };
