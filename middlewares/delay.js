bb49afe3422e865a0ad4d88a9665a384ba1c0682
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2018-07-10 13:33:48 -0300

    Remove duplicated code

--- middlewares/delay.js
+++ middlewares/delay.js
@@ -4,17 +4,10 @@ const sleep = delay => new Promise(resolve => setTimeout(resolve, delay));
 module.exports = ({ server, services }) => async (req, res, next) => {
   debug("delay");
   const { mockRequest } = req;
-  let delay = 0;
 
-  if (mockRequest) {
-    delay = mockRequest.delay || 0;
+  if (mockRequest && mockRequest.delay) {
+    await sleep(delay);
   }
 
-  if (delay === 0) {
-    next();
-    return;
-  }
-
-  await sleep(delay);
   next();
 };
--- middlewares/findDB.js
+++ middlewares/findDB.js
@@ -2,8 +2,6 @@ const debug = require("debug")("mock-server:middleware:findDB");
 const url = require("url");
 const { getConfig } = require("../config");
 
-const sleep = delay => new Promise(resolve => setTimeout(resolve, delay));
-
 module.exports = ({ server, services }) => async (req, res, next) => {
   debug("findDB");
   const { uriApi: URI_API } = getConfig();
