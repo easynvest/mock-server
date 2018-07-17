88f9e66a2a2aadc303d9a491bd1d7b719fa34cd7
Lucas de Castro <lucas.castro@ifactory.com>
2018-07-17 14:39:34 -0300

    Bufix cache only

--- bin/server.js
+++ bin/server.js
@@ -6,13 +6,12 @@ const mockServer = require('../app')
 
 const localPath = process.cwd()
 const { cacheOnly = false, mockServerConfigName = 'mock-server.conf.js' } = process.env
-
 const configFile = path.join(localPath, mockServerConfigName)
 
 try {
   // eslint-disable-next-line
   const config = require(configFile)
-  const app = mockServer(config, cacheOnly)
+  const app = mockServer(config, cacheOnly === 'true')
   const port = config.port || 3001
 
   app.set('port', port)
