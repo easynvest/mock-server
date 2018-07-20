7ea530f1a7c8ed9ad4230d71a8e2f30ff3353504
Lucas de Castro <lucascastro102@gmail.com>
2018-07-20 18:33:42 -0300

    Improve CLI (#33)
    * Create similar url search
    
    * 0.3.0
    
    * Create livereload
    
    * 0.3.1
    
    * Improve cli
    
    * Adiciona prepush task
    
    * Improve CLI
    
    * ignore some lines
    
    * Bugfix server path and config path
    
    * Lint rules and change init messages
    
    * Bufix cache only
    
    * Kill server on close
    
    * Kill server
    
    * Remove increase barlength
    
    * Remove filter bars

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
+/* eslint no-console:  ["error", { allow: ["log"] }] */
 
 const program = require('commander')
 const { prompt } = require('inquirer')
 const fs = require('fs')
 const path = require('path')
-const http = require('http')
-const mockServer = require('../app')
+const chalk = require('chalk')
+const kill = require('kill-port')
+const forever = require('forever-monitor')
 
 const localPath = process.cwd()
 
@@ -13,160 +15,186 @@ const questions = [
   {
     type: 'input',
     name: 'mockPath',
-    message: 'Deseja criar uma pasta para o mock-server...',
+    message: 'Inform the folder name you want use',
     default: './mock-server',
   },
   {
     type: 'input',
     name: 'port',
-    message: 'Informe a porta que deseja utiliar o mock...',
+    message: 'Please tell which port you want to use',
     default: '3001',
   },
   {
     type: 'input',
     name: 'api',
-    message: 'Informe a url da api...',
-  },
-  {
-    type: 'input',
-    name: 'resourcesPath',
-    message: 'Informa o local onde deseja armazenar os resources...',
-    default: './mock-server/resources',
+    validate: input => {
+      // eslint-disable-next-line no-useless-escape, max-len
+      const regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+      return !!input.trim() && regex.test(input)
+    },
+    message: 'Enter with the url API',
   },
 ]
+
+function startServer(cache = false, silent = false, fileName) {
+  return forever.start('server.js', {
+    uid: 'server',
+    watch: false,
+    silent,
+    env: {
+      cacheOnly: cache,
+      mockServerConfigName: fileName,
+    },
+    sourceDir: path.join(localPath, '/node_modules/@easynvest/mock-server/bin/'),
+  })
+}
+
+function infinitPrompt(props) {
+  // eslint-disable-next-line global-require, import/no-dynamic-require
+  const { port, uriApi } = require(path.join(localPath, props.configFile))
+  kill(port)
+
+  const message = chalk.green.bold('\n> Mock-Server is running: ')
+    + chalk.white(`http://localhost:${port}/proxy point to => ${uriApi}`)
+  const state = {
+    ...props,
+    silent: true,
+    message,
+    server: startServer(props.cacheOnly, true, props.configFile),
+  }
+
+  const killServer = () => {
+    if (state.server) state.server.stop()
+    kill(port)
+    process.exit(0)
+  }
+
+  const events = ['SIGINT', 'SIGTERM', 'exit']
+  events.forEach(event => {
+    process.on(event, killServer)
+  })
+
+  const startPrompt = () => {
+    console.log(
+      state.message,
+      chalk.white.bold('\n> Cache-only: ')
+        + chalk.yellow.bold(state.cacheOnly ? 'active' : 'disable'),
+      `${chalk.white.bold('\n> Show log:   ')
+        + chalk.yellow.bold(!state.silent ? 'active' : 'disable')}\n`,
+    )
+
+    const options = [
+      {
+        type: 'list',
+        name: 'option',
+        message: 'Options',
+        choices: [
+          {
+            value: 'd',
+            name: chalk.hex('#888b8d')(
+              ` Select to${state.cacheOnly ? ' Disable ' : ' Enable '}cache-only`,
+            ),
+          },
+          {
+            value: 'r',
+            name: chalk.hex('#888b8d')(' Select to Restart mock-server'),
+          },
+          {
+            value: 's',
+            name: chalk.hex('#888b8d')(
+              ` Select to${state.silent ? ' Enable log' : ' Disable log'}`,
+            ),
+          },
+          {
+            value: 'c',
+            name: chalk.hex('#888b8d')(' Select to close mock-server'),
+          },
+        ],
+      },
+    ]
+
+    prompt(options).then(response => {
+      process.stdout.write('\x1B[2J\x1B[0f')
+
+      switch (response.option) {
+        case 'r':
+          state.server.stop()
+          state.server = startServer(state.cacheOnly, state.silent, state.configFile)
+          break
+        case 'd':
+          state.cacheOnly = !state.cacheOnly
+          state.server.stop()
+          state.server = startServer(state.cacheOnly, state.silent, state.configFile)
+          break
+        case 's':
+          state.silent = !state.silent
+          state.server.stop()
+          state.server = startServer(state.cacheOnly, state.silent, state.configFile)
+          break
+        case 'c':
+          state.server.stop()
+          kill(port)
+          process.exit(0)
+          process.stdout.write('\x1B[2J\x1B[0f')
+          return
+        default:
+          return
+      }
+
+      startPrompt()
+    })
+
+    if (!state.silent) console.log('\n')
+  }
+  startPrompt()
+}
 
 program
   .command('init')
   .alias('i')
   .description('Generate a mock config')
   .action(() => {
-    prompt(questions).then(respostas => {
-      const mockPath = path.join(localPath, respostas.mockPath)
-      const resourcesPath = path.join(localPath, respostas.resourcesPath)
+    prompt(questions).then(responses => {
+      if (responses.mockPath) {
+        const mockPath = path.join(localPath, responses.mockPath)
 
-      if (!fs.existsSync(mockPath)) {
-        fs.mkdirSync(mockPath)
+        if (!fs.existsSync(mockPath)) {
+          fs.mkdirSync(mockPath)
+        }
+
+        const mockConfigPath = path.join(localPath, './mock-server.conf.js')
+        const mockConfigFile = fs.createWriteStream(mockConfigPath, {
+          flags: 'w',
+          encoding: 'utf-8',
+        })
+        const rewriteRoutesPath = path.join(mockPath, './rewriteRoutes.js')
+        const rewriteRoutesFile = fs.createWriteStream(rewriteRoutesPath, {
+          flags: 'w',
+          encoding: 'utf-8',
+        })
+
+        const jsonConfig = {
+          port: responses.port,
+          uriApi: 'swapi.co',
+          rewriteRoutes: `${responses.mockPath}/rewriteRoutes.js`,
+        }
+
+        mockConfigFile.end(`module.exports = ${JSON.stringify(jsonConfig, null, 2)}`)
+        rewriteRoutesFile.end("module.exports = {\n  '/api/*': '/$1'\n}\n")
       }
-
-      if (!fs.existsSync(resourcesPath)) {
-        fs.mkdirSync(resourcesPath)
-      }
-
-      const mockConfigFile = fs.createWriteStream(path.join(localPath, './mock-server.conf.js'), {
-        flags: 'w',
-        encoding: 'utf-8',
-      })
-      const rewriteRoutesFile = fs.createWriteStream(path.join(mockPath, './rewriteRoutes.js'), {
-        flags: 'w',
-        encoding: 'utf-8',
-      })
-
-      mockConfigFile.end(
-        `module.exports = {\n  port: '${respostas.port}',\n  uriApi: '${
-          respostas.api
-        }',\n  rewriteRoutes: '${respostas.mockPath}/rewriteRoutes.js',\n  resourcesPath: '${
-          respostas.resourcesPath
-        }'\n}\n`,
-      )
-      rewriteRoutesFile.end("module.exports = {\n  '/api/*': '/$1'\n}\n")
     })
   })
 
 program
   .command('start')
   .alias('s')
-  .description('Inicia mock-server')
-  .action(mockServerConfigName => {
-    const cacheOnly = program.cacheOnly || false
-    let newMockServerConfigName = ''
-
-    if (typeof mockServerConfigName === 'object') {
-      newMockServerConfigName = 'mock-server.conf.js'
-    }
-
-    const configFile = path.join(process.cwd(), newMockServerConfigName)
-
-    if (!fs.existsSync(configFile)) {
-      process.stdout.write(
-        `Não foi possivel encontrar o arquivo de configuração:\nArquivo: "${configFile}"`,
-      )
-      return
-    }
-
-    /**
-     * Normalize a port into a number, string, or false.
-     */
-
-    function normalizePort(val) {
-      const port = parseInt(val, 10)
-
-      // eslint-disable-next-line
-      if (isNaN(port)) {
-        // named pipe
-        return val
-      }
-
-      if (port >= 0) {
-        // port number
-        return port
-      }
-
-      return false
-    }
-    const config = require(configFile) //eslint-disable-line
-    const app = mockServer(config, cacheOnly)
-    const port = normalizePort(config.port)
-    app.set('port', port)
-
-    const server = http.createServer(app)
-
-    server.listen(port, () => {
-      process.stdout.write(
-        `JSON Server is running: http://localhost:${port}/ point to => ${config.uriApi}`,
-      )
-    })
-
-    server.on('error', onError) //eslint-disable-line
-    server.on('listening', onListening) //eslint-disable-line
-
-    /**
-     * Event listener for HTTP server "error" event.
-     */
-
-    function onError(error) {
-      if (error.syscall !== 'listen') {
-        throw error
-      }
-
-      const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`
-
-      // handle specific listen errors with friendly messages
-      switch (error.code) {
-        case 'EACCES':
-          process.stdout.write(`${bind} requires elevated privileges`)
-          process.exit(1)
-          break
-        case 'EADDRINUSE':
-          process.stdout.write(`${bind} is already in use`)
-          process.exit(1)
-          break
-        default:
-          throw error
-      }
-    }
-
-    /**
-     * Event listener for HTTP server "listening" event.
-     */
-
-    function onListening() {
-      const addr = server.address()
-      const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
-      process.stdout.write(`Listening on ${bind}`)
-    }
+  .description('Start mock-server')
+  .action(file => {
+    const configFile = typeof file === 'object' ? 'mock-server.conf.js' : file
+    infinitPrompt({ cacheOnly: program.cacheOnly, configFile })
   })
 
 program
   .version('0.1.0')
-  .option('--cache-only', 'Inicia mock sem fazer proxy para api')
+  .option('--cache-only', 'Start mock-server without proxy')
   .parse(process.argv)
--- middlewares/request.js
+++ middlewares/request.js
@@ -39,7 +39,9 @@ module.exports = () => async (req, res, next) => {
   } = req
 
   const parsedUrl = url.parse(originalUrl.replace(/^\/proxy/, ''))
-  const uri = `http://${URI_API}${parsedUrl.path}`
+  const uri = URI_API.includes('http')
+    ? `${URI_API}${parsedUrl.path}`
+    : `http://${URI_API}${parsedUrl.path}`
 
   try {
     const config = {
